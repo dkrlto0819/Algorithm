dda0c1745d3c33349cd1082c7355459b3c172d97
J <j@Jui-MacBookPro.local>
2019-05-09 21:36:56 +0900

    0509 study

--- 2019/Spring/crud/src/main/java/com/example/spring/study/crud/StudentController.java
+++ 2019/Spring/crud/src/main/java/com/example/spring/study/crud/StudentController.java
@@ -1,13 +1,39 @@
 package com.example.spring.study.crud;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/students")
 public class StudentController {
-    @Autowired //service 연결
+    @Autowired // service를 연결해준다
     private StudentService studentService;
+
+    @PostMapping // http method : post
+    public void create(@RequestBody Student student) { // request body의 student를 인식해라!
+        studentService.create(student);
+    }
+
+    @GetMapping
+    public List<Student> list() {
+        return studentService.list();
+    }
+
+    @GetMapping("/{id}")
+    public Student get(@PathVariable Integer id) {
+        return studentService.get(id);
+    }
+
+    @PutMapping
+    public void update(@RequestBody Student student) {
+        studentService.update(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        studentService.delete(id);
+    }
 
 }
--- 2019/Spring/crud/src/main/java/com/example/spring/study/crud/StudentService.java
+++ 2019/Spring/crud/src/main/java/com/example/spring/study/crud/StudentService.java
@@ -1,4 +1,32 @@
 package com.example.spring.study.crud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public void create(Student student) {
+        studentRepository.save(student);
+    }
+
+    public List<Student> list() {
+        return (List<Student>) studentRepository.findAll();
+    }
+
+    public Student get(Integer id) {
+        return studentRepository.findById(id).get();
+    }
+
+    public void update(Student student) {
+        studentRepository.save(student);
+    }
+
+    public void delete(Integer id) {
+        studentRepository.deleteById(id);
+    }
 }
